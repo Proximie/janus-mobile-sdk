2bd9dfeb92cfe4de458caca7737788e30fc81bff
Ghamza-Jd <hamzah.jadeed@gmail.com>
2025-03-04 10:27:28 +0200

    feat: added exist, generic event, and video room callbacks

--- rslib/src/plugins/videoroom/handle.rs
+++ rslib/src/plugins/videoroom/handle.rs
@@ -4,9 +4,9 @@ use crate::plugins::common::JanusId;
 use crate::protocol::Candidate;
 use crate::protocol::GenericEvent;
 use crate::protocol::Jsep;
-use jarust::plugins::audio_bridge::events::AudioBridgeEvent;
 use jarust::plugins::audio_bridge::events::PluginEvent;
 use jarust::plugins::video_room::handle::VideoRoomHandle as JaVideoRoomHandle;
+use jarust::plugins::video_room::params::VideoRoomExistsParams;
 use serde_json::Value;
 use std::fmt::Debug;
 use std::sync::Mutex;
@@ -35,4 +35,57 @@ impl VideoRoomHandle {
     }
 }
 
+#[uniffi::export(async_runtime = "tokio")]
+impl VideoRoomHandle {
+    pub async fn start_event_loop(&self, cb: Box<dyn VideoRoomHandleCallback>) {
+        if self.is_event_loop_running.load(Ordering::Relaxed) {
+            return;
+        }
+
+        let Ok(Some(mut receiver)) = self.receiver.lock().map(|mut x| x.take()) else {
+            return;
+        };
+
+        let join_handle = tokio::spawn(async move {
+            while let Some(event) = receiver.recv().await {
+                match event {
+                    PluginEvent::GenericEvent(generic_event) => {
+                        cb.on_handle_event(generic_event);
+                    }
+                    _ => {}
+                }
+            }
+        });
+
+        if let Ok(mut abort_handle) = self.abort_handle.lock() {
+            *abort_handle = Some(join_handle.abort_handle());
+        }
+        self.is_event_loop_running.store(true, Ordering::Relaxed);
+    }
+
+    pub async fn exist(
+        &self,
+        room_id: JanusId,
+        timeout: Duration,
+    ) -> Result<bool, JanusGatewayCommunicationError> {
+        match self
+            .inner
+            .exists(VideoRoomExistsParams { room: room_id }, timeout)
+            .await
+        {
+            Ok(rsp) => Ok(rsp),
+            Err(why) => Err(JanusGatewayCommunicationError::SendFailure {
+                reason: why.to_string(),
+            }),
+        }
+    }
+}
+
 base_handle_with_drop_impl!(VideoRoomHandle);
+
+#[uniffi::export(callback_interface)]
+pub trait VideoRoomHandleCallback: Send + Sync + Debug {
+    fn on_handle_event(&self, event: GenericEvent);
+    fn on_video_room_error(&self, error_code: u16, error: String);
+    fn on_other(&self, data: Vec<u8>);
+}
