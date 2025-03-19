746bd8db546f138bbaa917bd053ff4d8472c8eab
Ghamza-Jd <hamzah.jadeed@gmail.com>
2025-03-19 16:55:39 +0200

    refactor: updated the apple part of ownership transfer

--- apple-tracing-sub/README.md
+++ apple-tracing-sub/README.md
@@ -1,4 +1,4 @@
 # Apple Tracing Subscriber
 
-A tracing subscriber for apple platforms that's built on top of the native `oslog`. For the future we could use `os_signpost` to handle spans for better
-instrumentation.
+A tracing subscriber for apple platforms that's built on top of the native `oslog`.
+For the future we could use `os_signpost` to handle spans for better instrumentation.
--- rslib/src/logger.rs
+++ rslib/src/logger.rs
@@ -1,32 +1,32 @@
+#[cfg(target_os = "android")]
+#[uniffi::export]
+pub fn init_janus_logger() {
+    tracing_subscriber::fmt()
+        .with_ansi(false)
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive("jarust=trace".parse().unwrap()),
+        )
+        .compact()
+        .init();
+    android_logger::init_once(
+        android_logger::Config::default()
+            .with_max_level(log::LevelFilter::Trace)
+            .with_tag("JanusGateway"),
+    );
+    tracing::info!("Logger started");
+    log_panics::init();
+}
+
+#[cfg(any(target_os = "ios", target_os = "macos"))]
 #[uniffi::export]
 pub fn init_janus_logger(subsystem: &str, category: &str) {
-    #[cfg(target_os = "android")]
-    {
-        tracing_subscriber::fmt()
-            .with_ansi(false)
-            .with_env_filter(
-                tracing_subscriber::EnvFilter::from_default_env()
-                    .add_directive("jarust=trace".parse().unwrap()),
-            )
-            .compact()
-            .init();
-        android_logger::init_once(
-            android_logger::Config::default()
-                .with_max_level(log::LevelFilter::Trace)
-                .with_tag("JanusGateway"),
-        );
-    }
+    use apple_tracing_sub::subscriber::AppleTracingSubscriber;
+    use tracing_subscriber::layer::SubscriberExt;
 
-    #[cfg(any(target_os = "ios", target_os = "macos"))]
-    {
-        use apple_tracing_sub::subscriber::AppleTracingSubscriber;
-        use tracing_subscriber::layer::SubscriberExt;
-
-        let subscriber =
-            tracing_subscriber::registry().with(AppleTracingSubscriber::new(subsystem, category));
-        _ = tracing::subscriber::set_global_default(subscriber);
-    }
-
-    tracing::info!("JanusGateway started logging");
+    let subscriber =
+        tracing_subscriber::registry().with(AppleTracingSubscriber::new(subsystem, category));
+    _ = tracing::subscriber::set_global_default(subscriber);
+    tracing::info!("Logger started");
     log_panics::init();
 }
