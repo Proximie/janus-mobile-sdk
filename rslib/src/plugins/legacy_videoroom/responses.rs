498f7ed0a8871f2482a089ffdd24a2be2bfaceed
Ghamza-Jd <hamzah.jadeed@gmail.com>
2025-04-10 14:05:02 +0300

    feat(legacy-video-room): added create request

--- rslib/src/plugins/legacy_videoroom/responses.rs
+++ rslib/src/plugins/legacy_videoroom/responses.rs
@@ -1,1 +1,10 @@
+use crate::plugins::common::JanusId;
+use jarust::plugins::legacy_video_room::responses;
 
+pub type LegacyVideoRoomCreatedRsp = responses::LegacyVideoRoomCreatedRsp;
+
+#[uniffi::remote(Record)]
+pub struct LegacyVideoRoomCreatedRsp {
+    pub room: JanusId,
+    pub permanent: bool,
+}
